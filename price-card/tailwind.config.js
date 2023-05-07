220e50fb0095a4a96ca6746fcfa08ecffdb35e16
Neşathan Öztürk <nesathanozturk@Nesathan-MacBook-Pro.local>
2023-05-07 17:58:39 +0300

    added price card and column card

---
+++ price-card/tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  mode: "jit",
+  content: ["./**/*.{html}"],
+  theme: {
+    extend: {},
+    fontFamily: {
+      'poppins': ['"Poppins"'],
+    }
+  },
+  plugins: [],
+}
