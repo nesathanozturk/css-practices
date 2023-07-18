6d231534cef0aad2fc0026d1600efb4cb9bd1321
Neşathan Öztürk <nesathanozturk@Nesathan-MacBook-Pro.local>
2023-07-18 16:06:04 +0300

    feat: added huddle

---
+++ huddle/src/theme/index.js
@@ -0,0 +1,9 @@
+// Tailwind'daki gibi projenin her yerinde kullanabileceğimiz custom renkler tanımlayabiliyoruz.
+export const theme = {
+  colors: {
+    header: "#ebfbff",
+    body: "#fff",
+    footer: "#003333",
+  },
+  mobile: "768px",
+};
